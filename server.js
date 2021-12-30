342ca02158a1de95475cea8f422cdc1fccad571a
M.Elmonair <elmonair@yandex.com>
2021-12-30 19:58:37 +0500

    setup server and configuring express

--- server.js
+++ server.js
@@ -2,18 +2,25 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+// Setup Server // NOTE// Can use $env:port=5000 to change port code to which desired but defaut will 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Our server running on localhost port ${port}`);
+});
