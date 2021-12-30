0694ea89d1442f9de38b07c88efbc4ef94deab48
M.Elmonair <elmonair@yandex.com>
2021-12-30 22:41:51 +0500

    getTemp function

--- website/app.js
+++ website/app.js
@@ -1,5 +1,27 @@
 /* Global Variables */
-
+const generateBtn = document.querySelector('#generate');
+const temp = document.getElementById('temp');
+const date = document.getElementById('date');
+const feelings = document.getElementById('content');
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = '22d7980c9ec957151364a471a0fbeb99';
+// Event listener to add function to existing HTML DOM element
+const getTempData = async (postalCode) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?zip=${postalCode}&appid=${apiKey}&units=imperial`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const temp = data.main.temp;
+    console.log(temp);
+    return temp;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getTempData(11766);
